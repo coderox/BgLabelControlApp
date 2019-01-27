4c715d64d4dca3b254c32b4169d56bb1b614b8ba
Johan Lindfors <Johan.Lindfors@coderox.se>
2019-01-27 15:19:15 +0100

    Step 1 - Create a Blank App (BgLabelControlApp)

---
+++ BgLabelControlApp/BgLabelControl.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "BgLabelControl.h"
+
+namespace winrt::BgLabelControlApp::implementation
+{
+    hstring BgLabelControl::Label()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void BgLabelControl::Label(hstring const& value)
+    {
+        throw hresult_not_implemented();
+    }
+
+    Windows::UI::Xaml::DependencyProperty BgLabelControl::LabelProperty()
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ BgLabelControlApp/BgLabelControl.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "BgLabelControl.g.h"
+
+namespace winrt::BgLabelControlApp::implementation
+{
+    struct BgLabelControl : BgLabelControlT<BgLabelControl>
+    {
+        BgLabelControl() = default;
+
+        hstring Label();
+        void Label(hstring const& value);
+
+        static Windows::UI::Xaml::DependencyProperty LabelProperty();
+    };
+}
+
+namespace winrt::BgLabelControlApp::factory_implementation
+{
+    struct BgLabelControl : BgLabelControlT<BgLabelControl, implementation::BgLabelControl>
+    {
+    };
+}
