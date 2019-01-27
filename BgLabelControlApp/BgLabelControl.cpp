159df47551a2d3befc408de9bd691ab85746bf1f
Johan Lindfors <Johan.Lindfors@coderox.se>
2019-01-27 15:21:06 +0100

    Step 2 - Implement the BgLabelControl custom control class

--- BgLabelControlApp/BgLabelControl.cpp
+++ BgLabelControlApp/BgLabelControl.cpp
@@ -3,18 +3,22 @@
 
 namespace winrt::BgLabelControlApp::implementation
 {
-    hstring BgLabelControl::Label()
-    {
-        throw hresult_not_implemented();
-    }
+	Windows::UI::Xaml::DependencyProperty BgLabelControl::m_labelProperty =
+		Windows::UI::Xaml::DependencyProperty::Register(
+			L"Label",
+			winrt::xaml_typename<winrt::hstring>(),
+			winrt::xaml_typename<BgLabelControlApp::BgLabelControl>(),
+			Windows::UI::Xaml::PropertyMetadata{ winrt::box_value(L"default label"), Windows::UI::Xaml::PropertyChangedCallback{ &BgLabelControl::OnLabelChanged } }
+	);
 
-    void BgLabelControl::Label(hstring const& value)
-    {
-        throw hresult_not_implemented();
-    }
+	void BgLabelControl::OnLabelChanged(Windows::UI::Xaml::DependencyObject const& d, Windows::UI::Xaml::DependencyPropertyChangedEventArgs const& /* e */)
+	{
+		if (BgLabelControlApp::BgLabelControl theControl{ d.try_as<BgLabelControlApp::BgLabelControl>() })
+		{
+			// Call members of the projected type via theControl.
 
-    Windows::UI::Xaml::DependencyProperty BgLabelControl::LabelProperty()
-    {
-        throw hresult_not_implemented();
-    }
+			BgLabelControlApp::implementation::BgLabelControl* ptr{ winrt::get_self<BgLabelControlApp::implementation::BgLabelControl>(theControl) };
+			// Call members of the implementation type via ptr.
+		}
+	}
 }
--- BgLabelControlApp/BgLabelControl.h
+++ BgLabelControlApp/BgLabelControl.h
@@ -4,15 +4,27 @@
 
 namespace winrt::BgLabelControlApp::implementation
 {
-    struct BgLabelControl : BgLabelControlT<BgLabelControl>
-    {
-        BgLabelControl() = default;
+	struct BgLabelControl : BgLabelControlT<BgLabelControl>
+	{
+		BgLabelControl() { DefaultStyleKey(winrt::box_value(L"BgLabelControlApp.BgLabelControl")); }
 
-        hstring Label();
-        void Label(hstring const& value);
+		winrt::hstring Label()
+		{
+			return winrt::unbox_value<winrt::hstring>(GetValue(m_labelProperty));
+		}
 
-        static Windows::UI::Xaml::DependencyProperty LabelProperty();
-    };
+		void Label(winrt::hstring const& value)
+		{
+			SetValue(m_labelProperty, winrt::box_value(value));
+		}
+
+		static Windows::UI::Xaml::DependencyProperty LabelProperty() { return m_labelProperty; }
+
+		static void OnLabelChanged(Windows::UI::Xaml::DependencyObject const&, Windows::UI::Xaml::DependencyPropertyChangedEventArgs const&);
+
+	private:
+		static Windows::UI::Xaml::DependencyProperty m_labelProperty;
+	};
 }
 
 namespace winrt::BgLabelControlApp::factory_implementation
